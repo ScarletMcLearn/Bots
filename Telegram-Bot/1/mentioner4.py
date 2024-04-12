8eb8b1220358ed5a152c10fdad3996e12ee5227b
Scarlet McLearn <ratul_shams@yahoo.com>
2024-04-12 21:38:28 +0600

    Add files via upload

--- Telegram-Bot/1/mentioner4.py
+++ Telegram-Bot/1/mentioner4.py
@@ -31,14 +31,38 @@ async def send_messages():
         # # Send a private message to a user
         # await bot.send_message(chat_id='6878658109', text='Hello @GetRatulNow, this is a private message.')
 
+
+        message = """
+        Hey @GetRatulNow, the following tickets have comments which have not been rated:
+        1. Ticket 1: Make food
+        2. Ticket 2: Eat food 
+
+SQAA Board Update - 
+The following tickets have been in SQAA column (for 5 hours):
+        1.  QC : 2 tickets 
+        2. Prelive Deployed : 4 tickets 
+        3. Live Deployed: 1 ticket 
+
+Please look into these at the earliest. 
+
+Thank you.
+        """
+
         # Send a group message and tag a user
-        await bot.send_message(chat_id='-4191657913', text='Hello @GetRatulNow, this is a group message.')
+        await bot.send_message(chat_id='-4191657913', text=
+        message
+        # 'Hello @GetRatulNow, this is a group message.'
+        
+        )
 
 
 
         for user_id in user_ids:
             # Send a private message to each user
-            await bot.send_message(chat_id=user_id, text='Hello @GetRatulNow, this is a private message.')
+            await bot.send_message(chat_id=user_id, text=
+            message
+            # 'Hello @GetRatulNow, this is a private message.'
+            )
 
         # Wait for 5 seconds
         await asyncio.sleep(5)
